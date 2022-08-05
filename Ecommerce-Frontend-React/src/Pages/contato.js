18778f93dbce6b227d089748791ce01e96ee2ac3
LucasPaschoalotto <lucas.paschoaloto@hotmail.com>
2022-08-05 13:05:42 -0300

    Integracao do frontend com banco de dados/backend na secao de contato

--- Ecommerce-Frontend-React/src/Pages/contato.js
+++ Ecommerce-Frontend-React/src/Pages/contato.js
@@ -1,31 +1,33 @@
-import { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Grid, Button, TextField } from '@material-ui/core/';
 
 const Contatos = () => {
 
-    const url = 'http://localhost:5000/message'
+    const url = "http://localhost:5000/message";
+
     const [message, setMessage] = useState([]);
-    const [author, setAuthor] = useState('');
-    const [content, setContent] = useState('');
-    const [validator, setValidator] = useState(false);
+    const [author, setAuthor] = useState("");
+    const [content, setContent] = useState("");
+    const [validate, setValidate] = useState(false);
     const [render, setRender] = useState(false);
     const [success, setSuccess] = useState(false);
 
-    useEffect(async () => {
-        const response = await fetch(url)
-        const data = await response.json();
+    useEffect(async() => {
+        const res = await fetch(url);
+        const data = await res.json();
         setMessage(data);
-    }, [render])
-
-    const sendMessage = () => {
-        setValidator(false);
+    }, [render]);
+    
+    const sendMsg = () => {
+        setValidate(false);
         if(author.length <= 0 || content.length <= 0){
-            return setValidator(!validator)
-        }
+            return setValidate(!validate);
+        };
+
         const bodyForm = {
             email: author,
-            message: content,
-        }
+            message: content,        
+        };
 
         fetch(url, {
             method: "POST",
@@ -41,51 +43,50 @@ const Contatos = () => {
                 setSuccess(true);
                 setTimeout(() => {
                     setSuccess(false);
-                }, 5000)
+                }, 5000);
             }
-        })
-        
-        setAuthor('');
-        setContent('');
-        
-        console.log(content)
-    }  
+        });
+
+        setAuthor("");
+        setContent("");
+    };
 
     return(
         <>
             <Grid container direction="row" xs={12}>
-                <TextField id="name" label="Name" value={author} onChange={(event)=>{setAuthor(event.target.value)}} fullWidth/>
-                <TextField id="message" label="Message" value={content} onChange={(event)=>{setContent(event.target.value)}} fullWidth/>
+                <TextField id="name" label="Name" value={author} onChange={(event) => {setAuthor(event.target.value)}} fullWidth/>
+                <TextField id="message" label="Message" value={content} onChange={(event) => {setContent(event.target.value)}} fullWidth/>
             </Grid>
 
-            {validator && 
+            {validate && 
                 <div className="alert alert-warning alert-dismissible fade show mt-2" role="alert">
-                    <strong>Por favor preencha todos os campos!</strong>
-                    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    <strong>Favor, preencher todos os campos! </strong>
+                    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"/>
                 </div>
             }
 
             {success && 
                 <div className="alert alert-success alert-dismissible fade show mt-2" role="alert">
-                    <strong>Mensagem foi enviada</strong>
+                    <strong>Mensagem enviada! </strong>
                 </div>
             }
 
-            <Button onClick={sendMessage} className="mt-2" variant="contained" color="primary">
+            <Button onClick={sendMsg} className="mt-2" variant="contained" color="primary">
                 Sent
             </Button>
 
             {message.map((content) => {
                 return(
                     <div className="card mt-2" key={content.id}>
-                        <div className="card-body">
-                            <h5 className="card-title">{content.email}</h5>
-                            <p className="card-text">{content.message}</p>
-                            <p className="card-text"><small className="text-muted">{content.created_at}</small></p>
+                        <div class="card-body">
+                            <h5 class="card-title">{content.email}</h5>
+                            <p class="card-text">{content.message}</p>
+                            <p class="card-text"><small class="text-muted">{content.created_at}</small></p>
                         </div>
                     </div>
                 )
-            } )}
+            })
+            }
         </>
     )
 }
--- Ecommerce_Backend-NodeJS/src/server.ts
+++ Ecommerce_Backend-NodeJS/src/server.ts
@@ -2,12 +2,14 @@ import "reflect-metadata";
 import express from "express";
 import { router } from "./routes";
 import "./database";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(router);
 
 app.listen(5000, () => {
     console.log("Server on port 5000\nhttp://localhost:5000");
-})
+});
